8f471b794e8ac965364e17b9d0a40df8e1e0052b
georgeraja <rajageorge971@gmail.com>
2020-04-04 14:12:55 -0700

    implemented firebase utils includes ability to store users in database

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 import "./sign-in-and-sign-up.styles.scss";
 
 const SignInSignUpPage = () => (
   <div>
     <SignIn />
+    <SignUp />
   </div>
 );
 
