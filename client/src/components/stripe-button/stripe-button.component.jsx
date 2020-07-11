33f784bccaf3d050d07ffaaf3ecf3376d44403ec
georgeraja <rajageorge971@gmail.com>
2020-07-11 10:30:31 -0700

    seperating frontend and backend

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_iMXp8d1gotFCOD5Y5Qj1NeCi00AAwsvn0G";
+
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment was successful");
+      })
+      .catch((error) => {
+        console.log("payment error:", JSON.parse(error));
+        alert(
+          "There was an issue with your payment.Please make sure you use the provided credit card"
+        );
+      });
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Oxy Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
